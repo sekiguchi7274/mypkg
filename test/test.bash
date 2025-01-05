b3e2128ee67f12f044c47b951b0b8e9cf02ae311
sekiguchi <cyberakouxiang7274@gmail.com>
2025-01-05 11:46:47 +0900

    fix test.bash

--- test/test.bash
+++ test/test.bash
@@ -8,5 +8,4 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 30 ros2 launch mypkg talk_listen.launch.py | grep 'X'
-#wait $solar_flare_pid
+timeout 30 ros2 launch mypkg talk_listen.launch.py | sleep 3 | grep 'X'
